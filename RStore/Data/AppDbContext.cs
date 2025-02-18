644ad8ad4506b76d0858a684caaa9910f38ff0af
RafaDiogoJ <rafabbta@hotmail.com>
2025-02-17 22:28:58 -0300

    Ligação e criação de banco de dados

--- RStore/Data/AppDbContext.cs
+++ RStore/Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RStore.Models;
@@ -14,5 +15,88 @@ public class AppDbContext : IdentityDbContext<Usuario>
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<ProdutoFoto> ProdutoFoto { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Usuario>().ToTable("usuario");
+        builder.Entity<IdentityRole>().ToTable("perfil");
+        builder.Entity<IdentityUserRole<string>>().ToTable("usuario_perfil");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("usuario_regra");
+        builder.Entity<IdentityUserToken<string>>().ToTable("usuario_token");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("usuario_login");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");
+
+        #region Popular Categorias
+        List<Categoria> categorias = new() {
+            new() {
+                Id = 1,
+                Nome = "Eletronicos",
+            },
+            new(){
+                Id= 2,
+                Nome = "Celulares"
+            }
+        };
+        builder.Entity<Categoria>().HasData(categorias);
+        #endregion
+
+        #region Popular Usuario
+        Usuario usuario = new(){
+            Id = Guid.NewGuid().ToString(),
+            UserName = "Admin",
+            NormalizedUserName = "ADMIN",
+            Email = "rafabbta@hotmail.com",
+            NormalizedEmail = "RAFABBTA@HOTMAIL.COM",
+            EmailConfirmed = true,
+            Nome = "Rafael Diogo de Jesus",
+            DataNascimento = DateTime.Parse("27/12/1999"),
+            LockoutEnabled = true,
+        };
+        PasswordHasher<Usuario> password = new();
+        password.HashPassword(usuario, "123456");
+        builder.Entity<Usuario>().HasData(usuario);
+        #endregion   
+
+        #region Popular Perfil
+        List<IdentityRole> perfis = new(){
+            new(){
+                Id = Guid.NewGuid().ToString(),
+                Name = "Administrador",
+                NormalizedName = "ADMINISTRADOR"
+            },
+            new(){
+                Id = Guid.NewGuid().ToString(),
+                Name = "Funcionario",
+                NormalizedName = "FUNCIONARIO"
+            },
+            new(){
+                Id = Guid.NewGuid().ToString(),
+                Name = "Cliente",
+                NormalizedName = "CLIENTE"
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(perfis);
+        #endregion
+
+        #region Popular Usuario Perfil
+        List<IdentityUserRole<string>> userRoles = new(){
+            new(){
+                UserId = usuario.Id,
+                RoleId = perfis[0].Id
+            },
+            new(){
+                UserId = usuario.Id,
+                RoleId = perfis[1].Id
+            },
+            new(){
+                UserId = usuario.Id,
+                RoleId = perfis[2].Id
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
+    }
 
 }
--- RStore/Program.cs
+++ RStore/Program.cs
@@ -1,9 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RStore.Data;
+using RStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Serviço de Conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("RStoreConn");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+//Serviço de Gestao de Usuario - Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+//Garantir que o banco exista ao executar o projeto
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
